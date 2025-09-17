60c53501a819f44cb42a571444000feacde43145
unknown <rafaelcorrea.rc@gmail.com>
2025-09-17 19:23:13 -0300

    refactor: use `orchestrator.clearDatabase()`in tests

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import database from "infra/database";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -19,4 +20,10 @@ async function waitForAllServices() {
   }
 }
 
-export default { waitForAllServices };
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
+const orchestrator = { waitForAllServices, clearDatabase };
+
+export default orchestrator;
