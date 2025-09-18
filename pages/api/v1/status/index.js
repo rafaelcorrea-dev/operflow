73abd40d794e3630ba5c4d00bca466744f598c3b
Rafael Correa <111290427+rafaelcorrea17@users.noreply.github.com>
2025-09-18 15:36:31 -0300

    feat: create `status page`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -23,7 +23,7 @@ async function status(request, response) {
     databaseOpenedConnectionResult.rows[0].count;
 
   response.status(200).json({
-    update_at: updateAt,
+    updated_at: updateAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
