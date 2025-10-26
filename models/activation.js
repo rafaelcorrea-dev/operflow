de079e249b0e56ad6202bc9481eef4c030851df1
Rafael Correa <111290427+rafaelcorrea-dev@users.noreply.github.com>
2025-10-26 14:05:50 +0000

    feat: add activation tokens

---
+++ models/activation.js
@@ -0,0 +1,109 @@
+import database from "infra/database";
+import email from "infra/email.js";
+import webserver from "infra/webserver.js";
+import user from "models/user.js";
+
+const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutes
+
+async function findOneValidById(tokenId) {
+  const activationTokenObject = await runSelectQuery(tokenId);
+  return activationTokenObject;
+
+  async function runSelectQuery(tokenId) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          id = $1
+          AND expires_at > NOW()
+          AND used_at IS NULL
+        LIMIT
+          1
+        ;`,
+      values: [tokenId],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function markTokenAsUsed(activationTokenId) {
+  const usedActivationToken = await runUpdateQuery(activationTokenId);
+  return usedActivationToken;
+
+  async function runUpdateQuery(activationTokenId) {
+    const results = await database.query({
+      text: `
+        UPDATE
+          user_activation_tokens
+        SET
+          used_at = timezone('utc', now()),
+          updated_at = timezone('utc', now())
+        WHERE
+          id = $1
+        RETURNING
+          *
+        ;`,
+      values: [activationTokenId],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function activateUserByUserId(userId) {
+  const activatedUser = await user.setFeatures(userId, ["create:session"]);
+  return activatedUser;
+}
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const newToken = await runInsertQuery(userId, expiresAt);
+
+  return newToken;
+
+  async function runInsertQuery(userId, expiresAt) {
+    const results = await database.query({
+      text: `
+        INSERT INTO
+          user_activation_tokens (user_id, expires_at)
+        VALUES
+          ($1, $2)
+        RETURNING
+          *
+        ;`,
+      values: [userId, expiresAt],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function sendEmailToUser(user, activationToken) {
+  await email.send({
+    from: "Operflow <contato@operflow.com.br>",
+    to: user.email,
+    subject: "Ative seu cadastro",
+    text: `${user.username}, clique no link abaixo para ativar seu cadastro no Operflow:
+
+${webserver.origin}/cadastro/ativar/${activationToken.id}
+
+Atenciosamente,
+Equipe Operflow  
+    `,
+  });
+}
+
+const activation = {
+  findOneValidById,
+  markTokenAsUsed,
+  activateUserByUserId,
+  create,
+  sendEmailToUser,
+};
+
+export default activation;
--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -1,4 +1,7 @@
 import orchestrator from "tests/orchestrator.js";
+import activation from "models/activation.js";
+import user from "models/user.js";
+import webserver from "infra/webserver";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -8,6 +11,9 @@ beforeAll(async () => {
 });
 
 describe("Use case: Registration Flow (all successfull)", () => {
+  let createUserResonseBody;
+  let activationTokenId;
+
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -26,7 +32,7 @@ describe("Use case: Registration Flow (all successfull)", () => {
 
     expect(createUserResponse.status).toBe(201);
 
-    const createUserResonseBody = await createUserResponse.json();
+    createUserResonseBody = await createUserResponse.json();
 
     expect(createUserResonseBody).toEqual({
       id: createUserResonseBody.id,
@@ -37,5 +43,45 @@ describe("Use case: Registration Flow (all successfull)", () => {
       created_at: createUserResonseBody.created_at,
       updated_at: createUserResonseBody.updated_at,
     });
+  });
+
+  test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    expect(lastEmail.sender).toBe("<contato@operflow.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<registration.flow@curso.dev>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro");
+    expect(lastEmail.text).toContain("RegistrationFlow");
+
+    activationTokenId = orchestrator.extractUUID(lastEmail.text);
+
+    expect(lastEmail.text).toContain(
+      `${webserver.origin}/cadastro/ativar/${activationTokenId}`,
+    );
+
+    const activationTokenObject =
+      await activation.findOneValidById(activationTokenId);
+
+    expect(activationTokenObject.user_id).toBe(createUserResonseBody.id);
+    expect(activationTokenObject.used_at).toBe(null);
+  });
+
+  test("Activate account", async () => {
+    const activationResponse = await fetch(
+      `http://localhost:3000/api/v1/activations/${activationTokenId}`,
+      {
+        method: "PATCH",
+      },
+    );
+
+    expect(activationResponse.status).toBe(200);
+
+    const activationResponseBody = await activationResponse.json();
+
+    expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
+
+    const activatedUser = await user.findOneByUsername("RegistrationFlow");
+
+    expect(activatedUser.features).toEqual(["create:session"]);
   });
 });
